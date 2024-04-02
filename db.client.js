c0e2575c566102372ace0f7bc1804b906eda3ca2
Hydra75 <60604363+Hydra75@users.noreply.github.com>
2024-04-02 12:08:00 +0200

    Add files via upload

--- db.client.js
+++ db.client.js
@@ -1,6 +1,3 @@
-const { Sequelize } = require('sequelize')
-
-// database
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_name, process.env.DB_user, process.env.DB_password, {
