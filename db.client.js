48cfe085d1b07d361acba7ce1d3d164f6ed269da
Hydra75 <60604363+Hydra75@users.noreply.github.com>
2024-04-02 11:59:45 +0200

    Add files via upload

--- db.client.js
+++ db.client.js
@@ -1,17 +1,19 @@
 const { Sequelize } = require('sequelize')
 
 // database
-const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
-  {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_name, process.env.DB_user, process.env.DB_password, {
+  host: process.env.DB_host,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
   },
-);
+});
+
 
 // authentication and synchronization
 sequelize.authenticate()
